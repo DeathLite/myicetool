14b7a0295c25f73f54693b5504c53e904cc174dc
Robin <robin.l.07@live.fr>
2018-02-16 05:58:52 -0500

    Purge

--- app/Netatmo/Config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$client_id = "5a210976902a40f7a18bb836";
-$client_secret = "PhOYOp5HYTjLsvvg9n4PrhGtPueYObB8dkzIzIHRL4";
-$test_username = "dolbac66@gmail.com";
-$test_password = "comingsoon6612@";
-$scope= "read_station";
-?>
--- resources/views/layouts/form.blade.php
+++
@@ -1,9 +0,0 @@
-{!! Form::open(['url' => 'search']) !!}
-<div class="input-group searchBar">
-    {!! Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'Rechercher une ville', 'id' => 'location-input', 'autocomplete' => 'off']) !!}
-    <span class="input-group-btn">
-        {!! Form::submit('Search', ['class' => 'btn btn-primary']) !!}
-    </span>
-</div>
-{!! Form::close() !!}
-
